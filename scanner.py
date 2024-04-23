385dbbe451063be88b9d404c9e7300794c308ff4
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-04-23 06:02:01 +0300

    Updated scanner script to add creation dates to each bot account

--- scanner.py
+++ scanner.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import time
+from datetime import datetime
+from dateutil.tz import tzutc
 
 # Replace YOUR_BOT_TOKEN with your actual Discord bot token
 DISCORD_BOT_TOKEN = 'YOUR_BOT_TOKEN'
@@ -14,6 +16,12 @@ headers = {
 # Read the Discord user IDs from the input JSON file
 with open('data/ids.json', 'r') as file:
     user_ids = json.load(file)
+
+# Discord Epoch (Discord's Epoch is 2015-01-01T00:00:00.000Z)
+discord_epoch = datetime(2015, 1, 1, tzinfo=tzutc())
+
+# List to store user data
+user_data_list = []
 
 # Counter for entry numbers
 entry_number = 1
@@ -30,10 +38,18 @@ for user_id in user_ids:
         # Add entry number to user data
         user_data['entry_number'] = entry_number
 
-        # Append the user information to the output JSON file with a newline character
-        with open('data/scanner_output.json', 'a') as output_file:
-            json.dump(user_data, output_file, indent=4)
-            output_file.write('\n')  # Add a newline character
+        # Calculate the creation date from the Snowflake ID
+        snowflake_id = int(user_id)
+        creation_timestamp = (snowflake_id >> 22) + 1420070400000
+        creation_date = datetime.fromtimestamp(creation_timestamp / 1000, tz=tzutc())
+        user_data['creation_date'] = creation_date.strftime('%Y-%m-%d %H:%M:%S')
+
+        # Construct the avatar URL
+        avatar_url = f"https://cdn.discordapp.com/avatars/{user_id}/{user_data['avatar']}?size=1024"
+        user_data['avatar'] = avatar_url
+
+        # Append the user data to the list
+        user_data_list.append(user_data)
 
         print(f'Entry {entry_number}: Saved information for user ID {user_id}')
     else:
@@ -44,6 +60,10 @@ for user_id in user_ids:
 
     # Add a delay of 1 second between each request
     time.sleep(1)
+
+# Write the user data list to the output JSON file
+with open('data/scanner_output.json', 'a') as output_file:
+    json.dump(user_data_list, output_file, indent=4)
 
 # Inform that all users have been checked
 print('All entries have been processed. User information has been retrieved and saved to scanner_output.json')
